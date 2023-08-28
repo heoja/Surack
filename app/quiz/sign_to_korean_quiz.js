b72e26672e4fae1e77e6379baf22cf45f699e034
tiel2 <bigblue1122@gmail.com>
2023-08-28 17:29:54 +0900

    퀴즈 결과창 수정

--- app/quiz/sign_to_korean_quiz.js
+++ app/quiz/sign_to_korean_quiz.js
@@ -1,29 +1,32 @@
 "use client";
 import Image from "next/image"
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 
 import './quiz.css'
 import {useState} from "react"
 import rightsudal from '/public/suragy_happy2.png'
 import wrongsudal from "/public/suragy_sad.png"
+import cheerupSudal from "/public/suragy_cheerup.png"
+import floatingSudal from "/public/suragy_floating.png"
 import {quizdataS}  from "./data/data"
 
 
 
 export default function SignToKoreanQuiz({item}){
+  const router = useRouter()
+
   let [activeQuestion, setActiveQuestion] = useState(0);
   const { questions } = quizdataS;
   var topicName = item.substr(6)
   const topicQuestions = questions.filter(qList => qList.topic.includes(topicName));
   const { topic, question, answers, correctAnswer, image } = topicQuestions[activeQuestion];
   const questionLength = topicQuestions.length;
-  
+
   const [checkanswer, setcheckanswer] = useState(false);
   const [amIright, setamIright] = useState(false);
   const [Chosenanswer, setChosenanswer] = useState('')
   const [score, setscore] = useState(0)
-  // let chapter = "ch. 1:\u00a0\u00a0\u00a0\u00a0\u00a0"
-  // let chaptername = "첫 인사하기"
-  console.log(topicQuestions);
+
 
   const [finishedQuiz, setFinishedQuiz] = useState(false);
 
@@ -57,15 +60,18 @@ export default function SignToKoreanQuiz({item}){
       setamIright(false)};
   }
 
- 
-  
+  var goodScore = score >= questionLength / 2
   
   return(
     finishedQuiz ?
-    <div className="textbox-normal" style={{justifyContent:"center", alignItems:"center", height:"max-content"}}>Quiz done</div>
-    : <div>
+    <div  style={{alignItems:"center", justifyItems:"center", display:"flex", flexDirection:"column", height:"max-content"}}>
+      <Image src = {goodScore ? floatingSudal:cheerupSudal} style={{height:"40%", width:"auto"}}
+      alt = ""/>
+      <div style={{marginBottom:"10px"}}>{goodScore ? "잘했어용 ㅎㅎ🏆":"조금 더 노력해 보아요 ~💪"}{"\u00a0\u00a0("}{score}/{questionLength}{")"}</div>
+      <button className="btn" type="button" onClick={() => router.back()}>뒤로 가기</button>
+    </div>
       
-        
+    : <div>
           <h4 className = "quizhead" >{"💚\u00a0\u00a0"}{topicName}{"\u00a0\u00a0💚"}</h4>
       
         <div style={{alignItems:"center", justifyItems:"center", flexDirection:"row", display:"flex"}}>
@@ -85,11 +91,11 @@ export default function SignToKoreanQuiz({item}){
         {checkanswer? (amIright? 
         (<div style={{flex:"1"}}>
           <Image src = {rightsudal} className = "suragy_image" alt = "" height='200'/>
-          <div>정답입니다</div>
+          <div style={{marginTop:"0", marginBottom:"0"}}>정답입니다</div>
           </div>) : 
           (<div style={{flex:"1"}}>
             <Image src = {wrongsudal} className = "suragy_image" alt = "" height='200'/>
-            <div>아쉽네요~</div>
+            <div style={{marginTop:"0", marginBottom:"0"}}>아쉽네요~</div>
             </div>))
         : (<div></div>)}
 
